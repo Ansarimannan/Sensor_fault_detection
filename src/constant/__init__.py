9fdd5caa25e4bc7742a0f435b70979e4891df32b
Mannan <mohdmannanansari1@gmail.com>
2026-07-26 12:15:27 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,14 +1,15 @@
 import os
 
 AWS_S3_BUCKET_NAME=""
-MONGO_DATABASE_NAME=""
-MONGO_COLLECTION_NAME=""
+MONGO_DATABASE_NAME="pwskills"
+MONGO_COLLECTION_NAME="waferfault"
 
 
 TARGET_COLUMN=""
-MONGO_DB_URL=""
+MONGO_DB_URL="mongodb+srv://Mannan:12345@cluster0.t6vveqt.mongodb.net/?appName=Cluster0"
 
 
 MODEL_FILE_NAME=""
 MODEL_FILE_EXTENSION=""
 
+artifact_folder="artifacts"
